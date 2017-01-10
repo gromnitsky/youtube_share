7410879deecd76c1da209f3428e998e0cdacf758
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2017-01-11 01:16:41 +0200

    upload thumbnails to an ihs instead of using data URIs

---
+++ ext/ihs.js
@@ -0,0 +1,31 @@
+'use strict';
+
+let ihs = {};
+
+(function(exports) {
+
+    // for an Imgur-like service
+    let blob_upload = function(blob, url_post, auth_id) {
+	let fd = new FormData()
+	fd.append('type', 'file')
+	fd.append('image', blob)
+
+	return fetch(url_post, {
+	    method: 'POST',
+	    headers: { Authorization: `Client-ID ${auth_id}` },
+	    body: fd
+	}).then( res => {
+	    if (res.status !== 200) throw new Error(`status code: ${res.status}`)
+	    return res.json()
+	}).then( json => {
+	    if (!json.data || !json.data.link) {
+		console.log(json)
+		throw new Error('unknown json data in the response')
+	    }
+	    return json.data.link
+	})
+    }
+
+    exports.blob_upload = blob_upload
+
+})(typeof exports === 'object' ? exports : ihs)
--- test/canvas.js
+++ test/canvas.js
@@ -1,4 +1,4 @@
-/* global youtube_share */
+/* global youtube_share, ihs */
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -7,9 +7,21 @@ document.addEventListener('DOMContentLoaded', () => {
 	let ims = new youtube_share.ImgSealer(this)
 	ims.inject_to('#app')
 
-	let node = document.createElement("img")
-	node.src = ims.toBase64()
-	document.querySelector('#app').appendChild(node)
+//	let node = document.createElement("img")
+//	node.src = ims.toBase64()
+//	document.querySelector('#app').appendChild(node)
+
+	ims.toBlob().then( blob => {
+	    return ihs.blob_upload(blob, 'http://localhost:3000/3/image.json',
+				   'dc708f3823b7756')
+	}).then( link => {
+	    let node = document.createElement("img")
+	    node.src = link
+	    document.querySelector('#app').appendChild(node)
+	}).catch( err => {
+	    console.log(err)
+	})
+
     }
     fetch('data/rowan-atkinson-parrot.jpg')
 	.then( r => {

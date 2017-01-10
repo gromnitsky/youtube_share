c6d11e36d58e21540f4cffd3310ae65e84b01266
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2017-01-11 01:18:29 +0200

    ext: use ihs

--- ext/event_page.js
+++ ext/event_page.js
@@ -1,4 +1,6 @@
-/* globals chrome, youtube_share */
+/* globals chrome, youtube_share, ihs */
+
+let conf = {}
 
 let clipboard_write = function(str) {
     let node = document.querySelector('textarea')
@@ -8,16 +10,27 @@ let clipboard_write = function(str) {
 }
 
 let link_create = function(vid) {
+    let url_thumbnail = conf.youtube_frame.replace('%s', vid)
+    let ihs_provider = conf.ihs.imgur
+
     let img = new Image()
     img.onload = function() {
 	let imgsealer = new youtube_share.ImgSealer(this)
-	let data = imgsealer.toBase64()
-	let link = `<a href='https://www.youtube.com/watch?v=${vid}' target='_blank'><img src='${data}' alt='Opens a Youtube page'></a>`
 
-	clipboard_write(link)
-	alert(`A text for a Youtube share is copied into the clipboard (${link.length} KB)`)
+	imgsealer.toBlob().then( blob => {
+	    return ihs.blob_upload(blob,
+				   ihs_provider.url, ihs_provider.client_id)
+	}).then( link => {
+	    let r = `<a href='https://www.youtube.com/watch?v=${vid}' target='_blank'><img src='${link}' alt='Opens a Youtube page'></a>`
+
+	    clipboard_write(r)
+	    alert(`A text for a Youtube share is copied into the clipboard`)
+	}).catch( err => {
+	    alert(err)
+	})
+
     }
-    fetch(`https://i.ytimg.com/vi/${vid}/0.jpg`)
+    fetch(url_thumbnail)
 	.then( r => {
 	    if (r.ok) return r.blob()
 	    throw new Error('failed to fetch a thumbnail')
@@ -40,11 +53,23 @@ let click = function(info) {
     link_create(vid)
 }
 
-console.log('youtube_share: wake up')
-chrome.contextMenus.onClicked.addListener(click)
+let main = function(info) {
+    let config = info.installType === 'development' ? 'conf.debug.json' : 'conf.json'
+    console.log(`youtube_share: wake up; config=${config}`)
 
-// a stage area for clipboard
-document.body.appendChild(document.createElement("textarea"))
+    fetch(config).then( res => res.json()).then( json => {
+	conf = json
+	chrome.contextMenus.onClicked.addListener(click)
+	// a stage area for clipboard
+	document.body.appendChild(document.createElement("textarea"))
+
+    }).catch( err => {
+	console.log(err)
+    })
+}
+
+
+chrome.management.getSelf(main)
 
 // the callback shouldn't run each time chrome wakes up the extension
 chrome.runtime.onInstalled.addListener(() => {
